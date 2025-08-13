920bb296e845c5f23c9fd4da8c1871cda6e01374
Guillermo martinez <gmboggio@gmail.com>
2025-08-13 05:43:48 -0500

    Updated run_gfapp

--- R/run_gfapp.R
+++ R/run_gfapp.R
@@ -1,9 +1,20 @@
-#' Run the GreenFeed Shiny App on your computer
+#' @name run_gfapp
+#' @title Run the 'greenfeedr' Shiny App locally
+#'
+#' @description Launches the 'greenfeedr' Shiny application on your computer.
+#' The app provides an interactive interface for 'GreenFeed' data analysis, visualization, and reporting.
+#'
+#' @return This function launches the Shiny app in your default web browser; it does not return a value.
+#'
+#' @examples
+#' \dontrun{
+#'   greenfeedr::run_gfapp()
+#' }
 #'
 #' @export
 run_gfapp <- function() {
   appDir <- system.file("shinyapp", package = "greenfeedr")
-  if (appDir == "") {
+  if (appDir == "" || !dir.exists(appDir)) {
     stop("Could not find the Shiny app directory. Try reinstalling the package.")
   }
   shiny::runApp(appDir, display.mode = "normal")
