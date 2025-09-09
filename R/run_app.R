e56fea8a11a23193e4b3bffe8bea7cb4df42dce7
Guillermo martinez <gmboggio@gmail.com>
2025-09-09 15:04:41 -0700

    New version 1.3.1

--- R/run_app.R
+++
@@ -1,8 +0,0 @@
-#' Launch the Greenfeedr Shiny App locally
-#'
-#' This function launches the Greenfeedr Shiny app on your computer.
-#' @export
-run_app <- function() {
-  app_dir <- system.file("app", package = "greenfeedr")
-  shiny::runApp(app_dir)
-}
